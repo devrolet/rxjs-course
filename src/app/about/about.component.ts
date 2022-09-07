c8fb5c11b83c68b23d28e396b6bfe5728a5a571e
Chet Hill <chetdhill@gmail.com>
2022-09-07 11:39:31 -0500

    Initial Commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -27,6 +27,18 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
+        document.addEventListener('click', evt => console.log(evt));
+
+        let counter = 0; 
+
+        // setInterval(() => {
+            // console.log(counter);
+            // counter++;
+        // }, 1000);
+
+        setTimeout(() => {
+            console.log('finished...');
+        }, 3000);
 
     }
 
