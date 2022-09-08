877a058902914e8028d9c3b313d9333fe8e4e159
Chet Hill <chetdhill@gmail.com>
2022-09-08 12:30:15 -0500

    merge and mergeMap Operators

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,15 +26,14 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
-        const source1$ = of(1, 2, 3);
+        
+        const interval$ = interval(1000);
 
-        const source2$ = of(4, 5, 6);
+        const interval2$ = interval$.pipe(map(val => 10 * val));
 
-        const source3$ = of(7, 8, 9);
+        const result$ = merge(interval$, interval2$);
 
-        const result$ = concat(source1$, source2$, source3$);
-
-        result$.subscribe(console.log);
+        // result$.subscribe(console.log);
 
     }
 
